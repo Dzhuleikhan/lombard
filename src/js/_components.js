178e138d27e93ccb935f328dd18b7f28b2c0ead9
dzhuleikhan <dzhuleikhan@gmail.com>
2023-05-29 02:49:26 +0400

    Calculator functionality done

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import './components/_sliders'
 import './components/_select'
 import './components/_map'
+import './components/_calc'
---
+++ src/js/components/_calc.js
@@ -0,0 +1,52 @@
+let techSum = document.getElementById('tech-sum')
+let zaymAmount = document.getElementById('amountPerMonth')
+let monthly = document.getElementById('monthly')
+let daysInput = document.getElementById("days")
+let daysIndex = document.getElementById("days").selectedIndex;
+let option = document.getElementsByTagName("option")[daysIndex].value;
+let amountPerMonth = document.getElementById('amountPerMonth')
+let onHandAmount = document.getElementById('onHanAmount')
+const calcResetBtn = document.querySelector('.calc__reset')
+
+function calcReset() {
+  techSum.value = ''
+  zaymAmount.value = ''
+  onHandAmount.textContent = 0
+  monthly.textContent = 0
+}
+
+calcResetBtn.addEventListener('click', () => {
+  calcReset()
+})
+
+techSum.addEventListener('input', () => {
+  let a = techSum.value * 0.95
+  let b = zaymAmount.value = a
+  let daysIndex = document.getElementById("days").selectedIndex;
+  let option = document.getElementsByTagName("option")[daysIndex].value;
+  let monthlyPayment = b * 0.03
+  monthly.textContent = (monthlyPayment * option).toLocaleString()
+  onHandAmount.textContent = (techSum.value * 0.95).toLocaleString()
+})
+
+daysInput.addEventListener('change', () => {
+  let a = amountPerMonth.value
+  let daysIndex = document.getElementById("days").selectedIndex;
+  let option = document.getElementsByTagName("option")[daysIndex].value;
+  let monthlyPayment = a * 0.03
+  monthly.textContent = (monthlyPayment * option).toLocaleString()
+})
+
+amountPerMonth.addEventListener('input', () => {
+  if(amountPerMonth.value > techSum.value * 0.95) {
+    amountPerMonth.value =  techSum.value * 0.95
+  }
+  let a = amountPerMonth.value
+  let daysIndex = document.getElementById("days").selectedIndex;
+  let option = document.getElementsByTagName("option")[daysIndex].value;
+  let monthlyPayment = a * 0.03
+  monthly.textContent = (monthlyPayment * option).toLocaleString()
+})
+
+
+
